b3a43413dd94df99723505f4ab7df5b0fae17141
paco <franmenendezpouso@gmail.com>
2016-02-05 13:32:45 +0100

    clases Jugador y Juego

---
+++ Juego.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class Juego here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Juego
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<String>jugadores;
+    private int numerojugador;//numero de jugadores
+    /**
+     * Constructor for objects of class Juego
+     */
+    public Juego(int numero )
+    {
+        // initialise instance variables
+        numerojugador = numero;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    /**
+     * Método que reparte todas las cartas
+     * a los jugadores de forma habitual
+     * 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
