f33637aa4f6880453a53c156421eac17bc1eac27
noeliaalvarezprieto <noeliaalvarezprieto@gmail.com>
2016-02-07 18:44:00 +0100

    método repartir clase Juego

--- Juego.java
+++ Juego.java
@@ -1,7 +1,6 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Random;//por si acaso
 
 
 /**
@@ -16,8 +15,8 @@ public class Juego
     // ArrayList que almacena los jugadores de la partida.
     private ArrayList<Jugador>jugadores;
     private int numerojugador;//numero de jugadores
-    //mazo falta
-    
+    //mazo de cartas para jugar la partida
+     private Mazo mazo;
     /**
      * Constructor que cera un objeto de la clase juego,el cual crea
      * el número de jugadores indicados y crea el mazo con 50 cartas..
@@ -30,29 +29,29 @@ public class Juego
         // se inicia el juego con el número de jugadores y se crea el mazo.
         numerojugador = numero;
         jugadores = new ArrayList<Jugador>();
-        //mazo = new ArrayList<>();//?  la clase mazo(falta)
+        mazo = new Mazo ();
         int id = 1 ;
         if( (numero>2) && (numero<8))
         {
-           System.out.println("El número de jugadores es válido.Comienza la partida");
-            
-           while(id <= numero)
-           {
-             jugadores.add(new Jugador(id));
-             id = id +1;
+            System.out.println("El número de jugadores es válido.Comienza la partida");
+
+            while(id <= numero)
+            {
+                jugadores.add(new Jugador(id));
+                id = id +1;
             }
         }
         else
         {
-            
-           System.out. println("El número de jugadores no es válido,se crea un juego de 4 personas");
-           jugadores.add (new Jugador(1));
-           jugadores.add (new Jugador (2));
-           jugadores.add ( new Jugador(3));
-           jugadores.add (new Jugador(4));
+
+            System.out. println("El número de jugadores no es válido,se crea un juego de 4 personas");
+            jugadores.add (new Jugador(1));
+            jugadores.add (new Jugador (2));
+            jugadores.add ( new Jugador(3));
+            jugadores.add (new Jugador(4));
 
         }
-        
+
     }
 
     /**
@@ -62,20 +61,19 @@ public class Juego
      * Pueden quedar cartas en el mazo
      * 
      */
-    public void repartir()//n size()dice google 50
+    public void repartir()
     {
-        // utilizo shuffle para barajar o vale método barajar, de la clase mazo(falta)
-        Collections.shuffle(cartas);//
-        jugadores.clear();
-        int numCarta = 0;
-        int id = 1;
-        int mazo = 50;
-        while(numero < numCarta)
+        
+        mazo.barajar();//baraja las cartas
+        int mazo = 50;//numero de cartas a repartir
+        int numCarta = 0;// número de cartas para cada jugador
+        int id = 1;//id de cada jugador
+        
+        while(numCarta>(mazo/jugadores.size()))
         {
-            cartas.get(numCarta);//clase carta
-            id.add(cartas.get(numCarta));//algo con un metodo clase mazo
-            numCarta = numCarta + 1;//de las que tengo
-
+           jugadores.get(id).recibirCarta(mazo.tomarPrimera());//interger can´t.
+           id = id +1;
+           numCarta = numCarta + 1;
         }
     }
 
@@ -88,9 +86,9 @@ public class Juego
     {
         for(Jugador jugador :jugadores)
         {
-            system.out.println( "El id del jugador es " + jugador.getId() + " Y tiene " + jugador.cartasQueTieneEnLaMano());
+            System.out.println( "El id del jugador es " + jugador.getId() + " Y tiene " + jugador.cartasQueTieneEnLaMano());
         }
 
     }
-    
+
 }
